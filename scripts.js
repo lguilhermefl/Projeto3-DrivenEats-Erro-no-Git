e2de3e30c0eadcdb842b3a2675a2bb0065ff5d54
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-02 11:39:10 -0300

    adicionados comentários à função de marcar produtos

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 function marcarProduto (produto) {
 
+    //ARMAZENA PRODUTO SELECIONADO DA CATEGORIA CLICADA
     const selecionado = produto.parentElement.querySelector(".selecionado");
 
+    //VERIFICA SE O PRODUTO ESTÁ SELECIONADO
     if (selecionado !== null) {
+        //REMOVE A CLASSE SELECIONADO DO PRODUTO E ESCONDE MARCADOR
         selecionado.classList.remove("selecionado");
         selecionado.querySelector(".check").classList.add("escondido");
     } 
-
+    //VERIFICA SE NOVA SELEÇÃO DE PRODUTO É DIFERENTE DA ANTERIOR
     if (selecionado !== produto) {
+        //ADICIONA A CLASSE SELECIONADO DO PRODUTO E MOSTRA MARCADOR
         produto.classList.add("selecionado");
         produto.querySelector(".check").classList.remove("escondido");
     }
