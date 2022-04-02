5bbac3f695f336937661c772e0f2109e989a451b
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-02 11:52:00 -0300

    adicionadas variáveis globais de título e preço dos produtos e funcões para armazenar estes valores após seleção + comentários

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let tituloProduto;
+let preco;
+
 function marcarProduto (produto) {
 
     //ARMAZENA PRODUTO SELECIONADO DA CATEGORIA CLICADA
@@ -8,12 +11,19 @@ function marcarProduto (produto) {
         //REMOVE A CLASSE SELECIONADO DO PRODUTO E ESCONDE MARCADOR
         selecionado.classList.remove("selecionado");
         selecionado.querySelector(".check").classList.add("escondido");
+        //REDEFINE PARA UNDEFINED TÍTULO E VALOR DO PRODUTO DESMARCADO
+        tituloProduto = undefined;
+        preco = undefined;
     } 
     //VERIFICA SE NOVA SELEÇÃO DE PRODUTO É DIFERENTE DA ANTERIOR
     if (selecionado !== produto) {
         //ADICIONA A CLASSE SELECIONADO DO PRODUTO E MOSTRA MARCADOR
         produto.classList.add("selecionado");
         produto.querySelector(".check").classList.remove("escondido");
+        //ARMAZENA TÍTULO E VALOR DO PRODUTO SELECIONADO
+        tituloProduto = produto.querySelector(".titulo").innerText;
+        preco = produto.querySelector(".preco").innerText;
+        console.log(`${tituloProduto} + ${preco}`);
     }
 
 }
