1989b497454ae0934023c50d9ab11644a9871e64
luizdriven22@driven.com <luizdriven22@driven.com>
2022-04-02 11:33:14 -0300

    adicionada função de desmarcar produto marcado individualmente

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
-function marcarProduto(produto) {
+function marcarProduto (produto) {
 
     const selecionado = produto.parentElement.querySelector(".selecionado");
 
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
         selecionado.querySelector(".check").classList.add("escondido");
+    } 
+
+    if (selecionado !== produto) {
+        produto.classList.add("selecionado");
+        produto.querySelector(".check").classList.remove("escondido");
     }
-    
-    produto.classList.add("selecionado");
-    produto.querySelector(".check").classList.remove("escondido");
 
 }
